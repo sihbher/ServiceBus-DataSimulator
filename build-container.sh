ca695b44d5b3627309982a2d98cc0c16c3299ce3
Gerardo Reyes <gerardo.reyes@outlook.com>
2023-02-06 12:54:47 -0500

    Dockerfile name correction

--- build-container.sh
+++ build-container.sh
@@ -1,2 +1,2 @@
-docker build -t sihbher/sb-datasumulator:0.0.1 .
-docker push sihbher/sb-datasumulator:0.0.1
+docker build -t sihbher/sb-datasimulator:0.0.1 .
+docker push sihbher/sb-datasimulator:0.0.1
